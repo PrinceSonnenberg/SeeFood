0b40f6e0a2809bfe9dd534a7fbeb4d25c637d990
Prince Sonnenberg <princesonnenberg@users-macbook-pro.local>
2019-02-19 11:14:47 +0200

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Prince Sonnenberg on 2019/02/19.
+//  Copyright © 2019 Prince Sonnenberg. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+    }
+
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+}
+
